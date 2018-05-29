ffb9b8fe7f463f71551cdd82f8d033d28153bf76
Ruarai <ruarai.t@gmail.com>
2018-05-29 15:27:31 +1000

    improve code quality, add commenting

--- TimetablingApp/Controllers/HomeController.cs
+++ TimetablingApp/Controllers/HomeController.cs
@@ -17,15 +17,14 @@ namespace TimetablingApp.Controllers
     {
         private IHostingEnvironment _hostingEnvironment;
 
+        //The list of our subjects with some basic caching
         private static List<Subject> _subjectList = null;
         private List<Subject> subjectList
         {
             get
             {
                 if (_subjectList == null)
-                {
                     _subjectList = getSubjects();
-                }
                 
                 return _subjectList;
             }
@@ -41,31 +40,23 @@ namespace TimetablingApp.Controllers
             return View(subjectList);
         }
         
+        //Given a list of subject codes and sorting options, generates a list of possible timetables
         [HttpPost("/Home/BuildTimetable")]
         public async Task<IActionResult> BuildTimetable([FromBody] TimetableOptionsModel model)
         {
-            List<Subject> subjects = new List<Subject>();
-            foreach(var subjectCode in model.SubjectCodes)
-            {
-                Subject subject = subjectList.FirstOrDefault(s => s.Code == subjectCode);
-
-                if (subject == null)
-                    continue;
-
-                if(subject.ClassInfos == null)
-                    await subject.UpdateTimetable();
-
-                subjects.Add(subject);
-            }
+            List<Subject> subjects = await subjectsFromSubjectCodes(model.SubjectCodes);
 
             IEnumerable<ClassInfo> classInfos = subjects.SelectMany(subject => subject.ClassInfos);
             IEnumerable<ClassInfo> originalClassInfos = subjects.SelectMany(subject => subject.OriginalClassInfos);
 
+            //Assign unique ids to each of the 'original' class infos, allowing for compression to work later
             int id = 0;
             foreach(var classInfo in originalClassInfos)
                 classInfo.ID = id++;
 
+            //Create a new generator with our sorting options
             Generator g = new Generator { SortLaterStarts = model.LaterStarts, SortLessDays = model.LessDays };
+
             long possiblePermutations = Generator.PossiblePermutationsCount(classInfos);
 
             List<Timetable> timetables = new List<Timetable>();
@@ -76,40 +67,17 @@ namespace TimetablingApp.Controllers
             else
                 timetables = g.GenerateTimetablesBruteForce(classInfos);
 
-            var compressedTimetables = timetables.Select(t => new CompressedTimetable(t)).Take(25000).ToList();
+            //Take 25,000 of our timetables and compress them
+            var compressedTimetables = timetables.Take(25000).Select(t => new CompressedTimetable(t)).ToList();
                 
             return Json(new TimetableBuildResultModel(compressedTimetables, originalClassInfos.ToList()));
         }
 
-
-        public async Task<IActionResult> GetSubjectInfo(string subjectCode)
-        {
-            Subject testSubject = subjectList.FirstOrDefault(s => s.Code == subjectCode);
-
-            if (testSubject == null)
-                return Json(null);
-
-            await testSubject.UpdateTimetable();
-
-            return Json(testSubject.ClassInfos);
-        }
-
+        //Returns the number of possible permutations from a given list of subject codes,
         [HttpPost("/Home/UpdateSelectedSubjects")]
         public async Task<IActionResult> UpdateSelectedSubjects([FromBody]TimetableOptionsModel model)
         {
-            List<Subject> subjects = new List<Subject>();
-            foreach (var subjectCode in model.SubjectCodes)
-            {
-                Subject subject = subjectList.FirstOrDefault(s => s.Code == subjectCode);
-
-                if (subject == null)
-                    continue;
-
-                if (subject.ClassInfos == null)
-                    await subject.UpdateTimetable();
-
-                subjects.Add(subject);
-            }
+            List<Subject> subjects = await subjectsFromSubjectCodes(model.SubjectCodes);
 
             //No subjects? No possible timetables.
             if(!subjects.Any())
@@ -117,19 +85,44 @@ namespace TimetablingApp.Controllers
 
             IEnumerable<ClassInfo> classInfos = subjects.SelectMany(subject => subject.ClassInfos);
 
+            //Calculate the number of possible permutations
             long numPermutations = Generator.PossiblePermutationsCount(classInfos);
 
             return Json(numPermutations);
         }
 
+        private async Task<List<Subject>> subjectsFromSubjectCodes(List<string> subjectCodes)
+        {
+            //Find what subjects are selected from the given subject codes
+            List<Subject> subjects = new List<Subject>();
+            foreach (var subjectCode in subjectCodes)
+            {
+                Subject subject = subjectList.FirstOrDefault(s => s.Code == subjectCode);
+
+                //If the subject doesn't exist, continue
+                if (subject == null)
+                    continue;
+
+                //If we haven't loaded in the subject's timetable yet, do so
+                if (subject.ClassInfos == null)
+                    await subject.UpdateTimetable();
+
+                subjects.Add(subject);
+            }
+
+            return subjects;
+        }
+
+        //Fetches the subjects from disk and creates a list from them
         private List<Subject> getSubjects()
         {
             List<Subject> subjectList = new List<Subject>();
 
+            //Read in the list of subject codes and names from disk
             string subjectsText = System.IO.File.ReadAllText(Path.Combine(_hostingEnvironment.WebRootPath, "codes_2018_sem2.json"));
-
             var rawSubjects = JsonConvert.DeserializeObject<List<string>>(subjectsText);
 
+            //Create the list of subjects from the subject codes and names
             foreach (var rawSubject in rawSubjects)
             {
                 var parts = rawSubject.Split(' ');
@@ -140,6 +133,7 @@ namespace TimetablingApp.Controllers
                     Name = rawSubject.Substring(parts[0].Length + 1)
                 };
 
+                //Don't add duplicate subjects
                 if (subjectList.All(s => s.Code != subject.Code))
                     subjectList.Add(subject);
             }
