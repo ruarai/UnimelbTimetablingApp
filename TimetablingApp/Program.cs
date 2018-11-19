498f8f38f293c5d6e8bc094403c5fd4278959aa1
Ruarai <ruarai.big@gmail.com>
2018-11-19 23:15:41 +1100

    upgrade to aspnet core 2.1

--- TimetablingApp/Program.cs
+++ TimetablingApp/Program.cs
@@ -14,14 +14,13 @@ namespace TimetablingApp
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHost BuildWebHost(string[] args)
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
         {
             return WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
+                .UseStartup<Startup>();
 
         }
     }
